7a0a867cd0176905d8397ff252efb60460c59046
ejuelibm <60267192+ejuelibm@users.noreply.github.com>
2020-03-06 17:00:34 -0500

    Update cloud-init-apptier.tf

--- cloud-init-apptier.tf
+++ cloud-init-apptier.tf
@@ -1,4 +1,4 @@
-#Configuration to install nginx, a web server, on our VSIs when we provision them
+#This defines what to install while provisioning the Virtual Servers.  In this case it will install nginx which is a web server.
 
 data "template_cloudinit_config" "cloud-init-apptier" {
   base64_encode = false
