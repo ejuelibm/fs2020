4388b7b59999e602c1e5063920416a6e2c632d4b
ejuelibm <60267192+ejuelibm@users.noreply.github.com>
2020-03-06 16:54:48 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
+#This Configuration will create a VPC, set up 2 zones each with a subnet, and place a virtual instance in each as well as deploy a load
+#balancer attached to the servers. A simple cloud-init script will install nginx, to showcase an http response for proving out the example.
+#This variables.tf file sets up our variables for the Terraform Configuration
+
+#IBM Cloud Provider for Terraform Docs:  https://ibm-cloud.github.io/tf-ibm-docs/
+#IBM VPC CLI reference:  https://cloud.ibm.com/docs/infrastructure/vpc?topic=vpc-infrastructure-cli-plugin-vpc-reference
+
+
 variable "ibmcloud_region" {
   description = "Preferred IBM Cloud region to use for your infrastructure"
   default = "us-south"
@@ -33,11 +41,15 @@ variable "ssh_public_key" {
   description = "SSH Public Key contents to be used"
 }
 
+#Use 'ibmcloud is images list' command in IBM CLI to obtain a list of Images
+#See https://cloud.ibm.com/docs/cli?topic=vpc-infrastructure-cli-plugin-vpc-reference for IBM CLI usage
 variable "image" {
   default = "r006-14140f94-fcc4-11e9-96e7-a72723715315"
   description = "OS Image ID to be used for virtual instances"
 }
 
+#Use 'imbcloud is instance-profiles' command in IBM CLI to obtain list of Profiles
+#See https://cloud.ibm.com/docs/cli?topic=vpc-infrastructure-cli-plugin-vpc-reference for IBM CLI usage
 variable "profile" {
   default = "cx2-2x4"
   description = "Instance profile to be used for virtual instances"
