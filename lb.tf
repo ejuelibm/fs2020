b92cf1411fd8894ba4f3362932fa4cdc06e0d4d9
ejuelibm <60267192+ejuelibm@users.noreply.github.com>
2020-03-06 17:06:15 -0500

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -1,8 +1,12 @@
+#This file sets up our Load Balancer
+
+#Define our LB and it will cover Subnet 1 and Subnet 2
 resource "ibm_is_lb" "lb1" {
   name = "${var.vpc_name}-lb"
   subnets = ["${ibm_is_subnet.subnet1.id}","${ibm_is_subnet.subnet2.id}"]
 }
 
+#Define our LB pool using Round Robin and other details.
 resource "ibm_is_lb_pool" "lb1-pool" {
   lb = "${ibm_is_lb.lb1.id}"
   name = "${var.vpc_name}-lb1-pool"
@@ -15,6 +19,7 @@ resource "ibm_is_lb_pool" "lb1-pool" {
   health_monitor_url = "/"
 }
 
+#Provides a load balancer listener resource. This allows the load balancer listener to be created, updated, and cancelled
 resource "ibm_is_lb_listener" "lb1-listener" {
   lb = "${ibm_is_lb.lb1.id}"
   default_pool = "${element(split("/", ibm_is_lb_pool.lb1-pool.id),1)}"
@@ -22,6 +27,7 @@ resource "ibm_is_lb_listener" "lb1-listener" {
   protocol = "http"
 }
 
+#Tell our LB to send traffic to Instance 1
 resource "ibm_is_lb_pool_member" "lb1-pool-member1" {
   count = 1
   lb = "${ibm_is_lb.lb1.id}"
@@ -30,6 +36,7 @@ resource "ibm_is_lb_pool_member" "lb1-pool-member1" {
   target_address = "${ibm_is_instance.instance1.primary_network_interface.0.primary_ipv4_address}"
 }
 
+#Tell our LB to send traffic to Instance 2
 resource "ibm_is_lb_pool_member" "lb1-pool-member2" {
   count = 1
   lb = "${ibm_is_lb.lb1.id}"
