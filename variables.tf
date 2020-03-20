a17b435fbabbfc776e7b007af93c3942644de6b9
ejuelibm <60267192+ejuelibm@users.noreply.github.com>
2020-03-20 12:11:09 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,11 @@
 #IBM Cloud Provider for Terraform Docs:  https://ibm-cloud.github.io/tf-ibm-docs/
 #IBM VPC CLI reference:  https://cloud.ibm.com/docs/infrastructure/vpc?topic=vpc-infrastructure-cli-plugin-vpc-reference
 
+#The Description tags below are used by Schematics during the Variables area to help the user understand what the Variable is for.
+#The Default tag is the default value of the variable which can be overwritten by Schematics if the user wishes.
 
+#To change the region, use the IBM Cloud CLI tool and the command 'ibmcloud regions' to get a list of regions.  Remember that each Region
+#has it's own Image List with unique Image Identifiers so you will need to change that further down this file.
 variable "ibmcloud_region" {
   description = "Preferred IBM Cloud region to use for your infrastructure"
   default = "us-south"
@@ -41,7 +45,7 @@ variable "ssh_public_key" {
   description = "SSH Public Key contents to be used"
 }
 
-#Use 'ibmcloud is images list' command in IBM CLI to obtain a list of Images
+#Use 'ibmcloud is images list' command in IBM CLI to obtain a list of Images.  This command is Region specific.
 #See https://cloud.ibm.com/docs/cli?topic=vpc-infrastructure-cli-plugin-vpc-reference for IBM CLI usage
 variable "image" {
   default = "r006-14140f94-fcc4-11e9-96e7-a72723715315"
