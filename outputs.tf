1804ce25a6cf610d2cb8c57337798d8a054756e3
ejuelibm <60267192+ejuelibm@users.noreply.github.com>
2020-03-20 23:06:53 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,16 @@
+# This file contains the output variables that we want to see when the plan is executed and completed.
+
+#Floating IP address of Virtual Server 1
 output "FloatingIP-1" {
     value = "${ibm_is_floating_ip.floatingip1.address}"
 }
+
+#Floating IP address of Virtual Server 1
 output "FloatingIP-2" {
     value = "${ibm_is_floating_ip.floatingip2.address}"
 }
+
+#Hostname of our Load Balancer
 output "LB-Hostname" {
     value = "http://${ibm_is_lb.lb1.hostname}"
 }
